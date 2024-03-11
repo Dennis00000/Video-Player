25ba65332060208339c3ded7df0f9487226e581e
Dennis00000 <161707811+Dennis00000@users.noreply.github.com>
2024-03-11 02:08:23 +0200

    Add files via upload

---
+++ Video Player/Video Player 2.1/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Video_Player_2._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.listFiles = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(12, 13);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(471, 389);
+            this.axWindowsMediaPlayer1.TabIndex = 0;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(748, 379);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // listFiles
+            // 
+            this.listFiles.FormattingEnabled = true;
+            this.listFiles.ItemHeight = 16;
+            this.listFiles.Location = new System.Drawing.Point(652, 13);
+            this.listFiles.Name = "listFiles";
+            this.listFiles.Size = new System.Drawing.Size(229, 340);
+            this.listFiles.TabIndex = 2;
+            this.listFiles.SelectedIndexChanged += new System.EventHandler(this.listFiles_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(977, 436);
+            this.Controls.Add(this.listFiles);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Name = "Form1";
+            this.Text = "Video Player";
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.ListBox listFiles;
+    }
+}
+
---
+++ Video Player/Video Player 2.1/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Video_Player_2._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+         using(OpenFileDialog ofd=new OpenFileDialog() { Multiselect = true, ValidateNames = true, Filter = "WMV|*.wmv|WAV|*.wav|MP3|*.mp3|MP4|*.mp4|MKV|*.mkv"})
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    List<MediaFile> files = new List<MediaFile>();
+                    foreach (string filename in ofd.FileNames)
+                    {
+                        FileInfo fi = new FileInfo(filename);
+                        files.Add(new MediaFile() { FileName = Path.GetFileNameWithoutExtension(fi.FullName), Path = fi.FullName });
+                    }
+                    listFiles.DataSource = files;
+                }
+            }
+         }
+
+        private void listFiles_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            MediaFile file = listFiles.SelectedItem as MediaFile;
+            if (file != null)
+            {
+                axWindowsMediaPlayer1.URL = file.Path;
+                axWindowsMediaPlayer1.Ctlcontrols.play();
+            }
+        }
+      private void Form1_Load(object sender, EventArgs e)
+    {
+        listFiles.ValueMember = "Path";
+        listFiles.DisplayMember = "FileName";
+    }
+    }
+}
